dc642f4347cde38b85dd96e85eeede9c0bbc8a1a
Alausa Aishat <qtaisha.ao@gmail.com>
2022-06-15 13:52:56 +0100

    js objects lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Aisha',
+    streetAddress: '45 Adeoyo',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const Newemployee = {...employee};
+    Newemployee[key] = value;
+    return Newemployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const Newemployee = {...employee};
+    delete Newemployee[key];
+    return Newemployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
